da3f7e5213d178b143ae89e52bb5098c2ce35be4
Dmitry <dmitry27zh@gmail.com>
2023-12-08 17:28:01 +0500

    Add pageUrl state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import PokemonList from './PokemonList'
 
 function App() {
   const [pokemon, setPokemon] = useState(['bulbasaur', 'charmander'])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
+    axios.get(currentPageUrl).then((res) => {
+      setNextPageUrl(nextPageUrl)
+      setPrevPageUrl(prevPageUrl)
       setPokemon(res.data.results.map((p) => p.name))
     })
-  }, [])
+  }, [currentPageUrl])
 
   return <PokemonList pokemon={pokemon} />
 }
