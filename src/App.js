c2f2973252e8aab8a5d258205b1d24d769a243c9
Dmitry <dmitry27zh@gmail.com>
2023-12-08 17:31:05 +0500

    Add loading state

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ function App() {
   const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon')
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
+    setLoading(true)
     axios.get(currentPageUrl).then((res) => {
+      setLoading(false)
       setNextPageUrl(nextPageUrl)
       setPrevPageUrl(prevPageUrl)
       setPokemon(res.data.results.map((p) => p.name))
     })
   }, [currentPageUrl])
 
-  return <PokemonList pokemon={pokemon} />
+  if (loading) {
+    return 'Loading...'
+  } else {
+    return <PokemonList pokemon={pokemon} />
+  }
 }
 
 export default App
